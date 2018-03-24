ca1d1a16bcca0d428c3a1a0d67aa64d9feba3fb8
Jason Wessels <jwessels71@live.com>
2018-03-24 10:37:02 -0700

    More cleanup.

--- src/generate_protobuf.cmd
+++ src/generate_protobuf.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-packages\Google.Protobuf.Tools.3.5.1\tools\windows_x64\protoc.exe --csharp_out=. azbackup.proto
+packages\Google.Protobuf.Tools.3.5.1\tools\windows_x64\protoc.exe --csharp_out=. azbackup_proto.proto
